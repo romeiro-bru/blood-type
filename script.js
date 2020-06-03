b34e761460c08cacf154a79c9ad479d544034291
bruna romeiro <bruna_romeiro@yahoo.com.br>
2020-06-02 21:20:25 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+function compatibilidade() {
+    var doador = document.getElementById('doador-type')
+    var receptor = document.getElementById('receptor-type')
+    var resultado = document.querySelector('div#resultado')
+
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+
+    if (doador.value == "O") {       
+        if (receptor.value == "O" || receptor.value == "A") {
+            resultado.innerHTML = 'Os tipos sanguíneos são compatíveis!'
+            img.setAttribute('src', 'happy-drop.png')
+        } else {
+            resultado.innerHTML = 'Os tipos sanguíneos são compatíveis!'
+            img.setAttribute('src', 'happy-drop.png')
+        }
+    } else if (doador.value == "A") {
+        if (receptor.value == "A" || receptor.value == "AB") {
+            resultado.innerHTML = 'Os tipos sanguíneos são compatíveis!'
+            img.setAttribute('src', 'happy-drop.png')
+        } else {
+            resultado.innerHTML = 'Os tipos sanguíneos não são compatíveis.'
+            img.setAttribute('src', 'sad-drop.png')
+        }
+    } else if (doador.value == "B") {
+        if (receptor.value == "B" || receptor.value == "AB") {
+            resultado.innerHTML = 'Os tipos sanguíneos são compatíveis!'
+            img.setAttribute('src', 'happy-drop.png')
+        } else {
+            resultado.innerHTML = 'Os tipos sanguíneos não são compatíveis.'
+            img.setAttribute('src', 'sad-drop.png')
+        }        
+    } else {
+        if (receptor.value == "AB") {
+            resultado.innerHTML = 'Os tipos sanguíneos são compatíveis!'
+            img.setAttribute('src', 'happy-drop.png')
+        } else {
+            resultado.innerHTML = 'Os tipos sanguíneos não são compatíveis.'
+            img.setAttribute('src', 'sad-drop.png')
+        }
+    }
+    resultado.appendChild(img)
+}
